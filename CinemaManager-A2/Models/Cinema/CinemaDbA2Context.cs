34f5e6647b782a6167a5ea28bab351fbd7030d0e
firas1996 <firas.hm.1996@gmail.com>
2025-10-27 10:56:51 +0100

    abc

--- CinemaManager-A2/Models/Cinema/CinemaDbA2Context.cs
+++ CinemaManager-A2/Models/Cinema/CinemaDbA2Context.cs
@@ -15,6 +15,8 @@ public partial class CinemaDbA2Context : DbContext
     {
     }
 
+    public virtual DbSet<Movie> Movies { get; set; }
+
     public virtual DbSet<Producer> Producers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -22,6 +24,25 @@ public partial class CinemaDbA2Context : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Movie__3214EC072D6F6B3F");
+
+            entity.ToTable("Movie");
+
+            entity.Property(e => e.Genre)
+                .HasMaxLength(20)
+                .IsFixedLength();
+            entity.Property(e => e.Title)
+                .HasMaxLength(30)
+                .IsFixedLength();
+
+            entity.HasOne(d => d.Producer).WithMany(p => p.Movies)
+                .HasForeignKey(d => d.ProducerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Movie_Prod");
+        });
+
         modelBuilder.Entity<Producer>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__Producer__3214EC078C15559B");
